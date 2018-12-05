4f0ba6bdceef9af1a6e7dcc88f66e7ecdb9e4275
ramanaganesh <ramanaganesh08@gmail.com>
2018-12-05 18:53:23 +0530

    servlet

--- Servlet/src/com/bridgelabz/controller/LoginUser.java
+++ Servlet/src/com/bridgelabz/controller/LoginUser.java
@@ -14,9 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LoginUser extends HttpServlet
 {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	
     public void init() throws ServletException 
